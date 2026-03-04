b79ee8eb65b680cb6bbba5265e27c972e6b7464a
AHMAD-SANI <ahmadsani7469@gmail.com>
2026-03-03 16:25:55 -0800

    middleware error

--- CHATFLOW/settings.py
+++ CHATFLOW/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middlewere.WhiteNoiseMiddlewere'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
